0a35e5f0c8c0ae6e7532f2caf750506368f49578
KavishRazdan <110294845+KavishRazdan@users.noreply.github.com>
2026-03-25 22:23:08 +0530

    es lint fix

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -16,6 +16,7 @@ const AppContextProvider = (props) => {
       document.getElementById("body").style.backgroundColor = "#121212";
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
+  
   }, [window.location.pathname]);
 
   return (
